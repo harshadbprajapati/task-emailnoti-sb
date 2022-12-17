1c2cc06eaf74b9c0c8eaafbaa20cf17ae0b82153
Meet Dadhania <dadhaniameet1744@gmail.com>
2022-12-17 17:54:39 +0530

    Navbar Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar1 from './Components/navbar';
+import Navresponce from './Components/nav';
+import Home from './Components/Home';
 
 const App = () => {
   return(
     <>
-      <Navbar1 />
+      <Navresponce />
+      <Home />
     </>
   )
 }
