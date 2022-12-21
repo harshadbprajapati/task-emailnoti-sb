89a3c1f9be45603988df06f7422e0c36b05b508e
Meet Dadhania <dadhaniameet1744@gmail.com>
2022-12-22 00:13:06 +0530

    Form

--- frontend/src/Components/form.js
+++ frontend/src/Components/form.js
@@ -23,7 +23,7 @@ const Displayform =()=>{
     }
 
     const studentRegistered=(data)=>{
-        axios.post(`${baseurl}/`,data).then(
+        axios.post(`${baseurl}/studentdetails`,data).then(
             (response)=>{
                 console.log(response)
             },
@@ -43,27 +43,30 @@ const Displayform =()=>{
                     <div class="content_box">
                         <form onSubmit={handleForm}>
                         <p>
-                            Name: <br />
-                            <input type="text" name="Name" id="Name" placeholder="Type your full name here" required onChange={(e)=>{
-                                setStudent({...student,student_name:e.target.value})
-                            }}/>
-                        </p>
-                        <p>
-                            Email: <br />
-                            <input type="email" placeholder="Type your email here" required onChange={(e)=>{
-                                setStudent({...student,student_email:e.target.value})
+                            ID: <br />
+                            <input type="text" name="text" id="Address" placeholder="Type your ID here" required onChange={(e)=>{
+                                setStudent({...student,studentId:e.target.value})
                             }}/>
                         </p>
                         <p>
                             Contact No.: <br />
                             <input type="tel" name="mobile" id="mobile" placeholder="Contact No." required onChange={(e)=>{
-                                setStudent({...student,student_contact_number:e.target.value})
+                                setStudent({...student,studentContactNumber:e.target.value})
                             }}/>
                         </p>
                         <p>
-                            ID: <br />
-                            <input type="text" name="text" id="Address" placeholder="Type your ID here" required onChange={(e)=>{
-                                setStudent({...student,student_email:e.target.value})
+                            Name: <br />
+                            <input type="text" name="Name" id="Name" placeholder="Type your full name here" required onChange={(e)=>{
+                                setStudent({...student,studentName:e.target.value})
+                            }}/>
+                        </p>
+                        
+                        
+                        
+                        <p>
+                            Email: <br />
+                            <input type="email" placeholder="Type your email here" required onChange={(e)=>{
+                                setStudent({...student,studentEmail:e.target.value})
                             }}/>
                         </p>
                             <input type="submit" value="Done" />
--- frontend/src/api/bootapi.js
+++ frontend/src/api/bootapi.js
@@ -1,2 +1,2 @@
-const baseurl = "http://localhost:8080";
+const baseurl = "http://192.168.91.56:8080";
 export default baseurl;
